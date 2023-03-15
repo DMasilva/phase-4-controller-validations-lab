e56859741559235e697a11300d58c3cfc871593e
David <david.owuor@student.moringaschool.com>
2023-03-16 01:07:09 +0300

    complete

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,21 +1,37 @@
 class AuthorsController < ApplicationController
-  
+
+
+  rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity
+
   def show
     author = Author.find(params[:id])
 
     render json: author
   end
 
-  def create
-    author = Author.create(author_params)
+  # def create
+  #   author = Author.create(author_params)
 
+  #   render json: author, status: :created
+
+  # end
+
+  def create
+    author = Author.create!(author_params)
     render json: author, status: :created
+  rescue ActiveRecord::RecordInvalid => invalid
+    render json: { errors: invalid.record.errors }, status: :unprocessable_entity
   end
 
   private
-  
+
   def author_params
     params.permit(:email, :name)
   end
-  
+
+
+  def render_unprocessable_entity(invalid)
+    render json: { errors: invalid.record.errors.full_messages }, status: :unprocessable_entity
+  end
+
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,23 +1,38 @@
 class PostsController < ApplicationController
 
+
+  #rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity
+
   def show
     post = Post.find(params[:id])
-    
+
     render json: post
   end
 
+  # def update
+  #   post = Post.find(params[:id])
+
+  #   post.update(post_params)
+
+  #   render json: post
+  # end
+
   def update
     post = Post.find(params[:id])
-
-    post.update(post_params)
-
+    post.update!(post_params)
     render json: post
+  rescue ActiveRecord::RecordInvalid => invalid
+    render json: { errors: invalid.record.errors }, status: :unprocessable_entity
   end
 
   private
 
   def post_params
     params.permit(:category, :content, :title)
+  end
+
+  def render_unprocessable_entity(invalid)
+    render json: { errors: invalid.record.errors }, status: :unprocessable_entity
   end
 
 end
